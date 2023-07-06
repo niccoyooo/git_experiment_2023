9243d950e8e1917b8bd215eb862262152ebc1ae7
niccoyooo <niccolojc16@gmail.com>
2023-07-06 10:37:59 -0400

    trying this out

---
+++ testingGit.py
@@ -0,0 +1,19 @@
+
+
+class Colin:
+    def __init__(self, height, hair, drum_capability, health, location):
+        height = self.height
+        hair = self.hair
+        drum_capability = self.drum_capability
+        health = self.health
+        location = self.location
+
+    def haircut (self):
+        hair = False
+    
+    def height_reduction(self):
+        height -= 5
+
+Colon = Colin(40, True, True, "Nominal", "God knows where...")
+
+Colon = Colon.haircut()
