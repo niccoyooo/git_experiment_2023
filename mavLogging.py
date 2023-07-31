bf4e35a487b240f9e39dddf2ff6c65fe14d837eb
niccoyooo <niccolojc16@gmail.com>
2023-07-31 11:19:26 -0400

    New file to log using RC_OVERRIDE_CHANNELS

---
+++ mavLogging.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+from pymavlink import mavutil
+
+class Bridge(object):
+    """ MAVLink bridge
+
+    Attributes:
+        conn (TYPE): MAVLink connection
+        data (dict): Deal with all data
+    """
+    def __init__(self, device='udp:192.168.9.49:14550', baudrate=921600):
+        """
+        Args:
+            device (str, optional): Input device
+                https://ardupilot.github.io/MAVProxy/html/getting_started/starting.html#master
+            baudrate (int, optional): Baudrate for serial communication
+        """
+        self.conn = mavutil.mavlink_connection(device, baud=baudrate)
+        self.data = {}
+
+    def get_data(self):
+        """ Return data
+
+        Returns:
+            TYPE: Dict
+        """
+        return self.data
+
+    def get_all_msgs(self):
+        """ Return all mavlink messages
+
+        Returns:
+            TYPE: dict
+        """
+        msgs = []
+        while True:
+            msg = self.conn.recv_match()
+            if msg != None:
+                msgs.append(msg)
+            else:
+                break
+        return msgs
+
+    def update(self):
+        """ Update data dict
+        """
+        # Get all messages
+        msgs = self.get_all_msgs()
+        # Update dict
+        for msg in msgs:
+            self.data[msg.get_type()] = msg.to_dict()
+
+    def print_data(self):
+        """ Debug function, print data dict
+        """
+        print(self.data)
+
+    def set_rc_channel_pwm(self, id, pwm=1100):
+        """ Set RC channel pwm value
+
+        Args:
+            id (TYPE): Channel id
+            pwm (int, optional): Channel pwm value 1100-2000
+        """
+        rc_channel_values = [65535 for _ in range(8)]
+        rc_channel_values[id] = pwm
+        #http://mavlink.org/messages/common#RC_CHANNELS_OVERRIDE
+        self.conn.mav.rc_channels_override_send(
+            self.conn.target_system,                # target_system
+            self.conn.target_component,             # target_component
+            *rc_channel_values)                     # RC channel list, in microseconds.
+
+if __name__ == '__main__':
+    bridge = Bridge()
+    #bridge = Bridge(device='udp:192.169.9.49:14550')
+    while True:
+        bridge.update()
+        bridge.print_data()
